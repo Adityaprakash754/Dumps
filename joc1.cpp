3ddd1ec114464e7558dc1a5e24969f4dc23006dd
Aditya Prakash <112635806+Adityaprakash754@users.noreply.github.com>
2023-08-22 01:51:34 +0530

    JOC1 all test case passed

---
+++ joc1.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cout << "Enter no test cases" << endl;
+    cin >> t;
+    
+    while(t--){
+        float a_pur, ret;
+        int ap,n,ch;
+        cout << "Enter amount of purchase and amount paid" << endl;
+        cin >> a_pur;
+        cin >> ap;
+        
+        ret = ap - a_pur;
+        
+        cout << "Enter no of denominations" << endl;
+        cin >> n;
+        
+        pair <float,int> p[n];
+        cout << "Enter denomination and it's count" << endl;
+        for(int i = 0; i < n; i++){
+            cin >> p[i].first;
+            cin >> p[i].second;
+        }
+        
+        
+        cout << "Enter 1: for more 2: for less change " << endl;
+        cin >> ch;
+        
+        int soln [n] = {0};
+        float temp = ret;
+            
+        if(ch == 1){
+            sort(p, p + n);
+            
+            int i = 0;
+            while(temp > 0){
+                while(temp > 0 && p[i].second > 0){
+                    soln[i]++ ;
+                    p[i].second--;
+                    temp -= p[i].first; 
+                }
+                i++;
+            }
+            
+            
+            if(temp < 0){
+                i = i - 2;
+                temp = temp * (-1);
+                while(temp != 0){
+                    int x = temp / p[i].first;
+                    if(x > 0){
+                        soln[i] -= x;
+                        p[i].second += x;
+                        temp -= x*p[i].first;
+                    }
+                    else
+                        i--;
+                }
+            }
+        }
+        
+        else if(ch == 2){
+            sort(p, p + n, greater < pair <float, int> > ());
+            
+            int i = 0;
+            while(temp > 0){
+                while(temp > 0 && p[i].second > 0 && temp - p[i].first >= 0){
+                    soln[i]++ ;
+                    p[i].second--;
+                    temp -= p[i].first; 
+                }
+                i++;
+            }
+        }    
+        
+        cout << "Change to be returned: "<< ret << endl;
+        for(int j = 0; j < n; j++){
+            if(soln [j] != 0){
+                cout << "Rs " << p[j].first << " = " << soln[j] << endl;
+            }
+        }
+    }
+    return 0;
+}
